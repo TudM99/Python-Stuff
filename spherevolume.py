8cda7fa94b10d83541590d80911e8b0f92901bcf
TudM99 <67870163+TudM99@users.noreply.github.com>
2023-02-25 03:05:51 +0200

    Add files via upload

---
+++ spherevolume.py
@@ -0,0 +1,13 @@
+import math
+def volume(r):
+	v = ((4/3)*(math.pi))*(r**3)
+	print (v)
+
+print ("The sphere's radius = ", end="") 
+
+r = int(input())
+while r == 0:
+	print ("The sphere's variable must be positive")
+	print ("Please re-enter the value: ", end="")
+	r = int(input())
+volume(r)
